03b0f5467d9830039a293f9b00ee12c1661d935c
Vadim Pashkin <vaplev@mail.ru>
2020-06-30 18:15:29 +0300

    testing...

--- src/Game_Land.js
+++ src/Game_Land.js
@@ -1,7 +1,9 @@
 import Game_Land from './Game_Land.html'
-import './css/Game_Land.css'
 import './images/bg-about.png'
 import './images/bg-features.png'
 import './images/bg-main.svg'
 import './images/bg-qoutes.png'
 import './images/bg-requirements.svg'
+import './css/Game_Land.css'
+import '@fortawesome/fontawesome-free/js/all'
+import "@fortawesome/fontawesome-free/css/all.min.css"

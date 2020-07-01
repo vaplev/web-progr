d6836f291d73ce7371b227e75aef532750db2e85
Vadim Pashkin <vaplev@mail.ru>
2020-07-01 21:48:05 +0300

    before installed smth (just in case)

--- src/Game_Land.js
+++ src/Game_Land.js
@@ -4,6 +4,13 @@ import './images/bg-features.png'
 import './images/bg-main.svg'
 import './images/bg-qoutes.png'
 import './images/bg-requirements.svg'
+import './images/statue.svg'
 import './css/Game_Land.css'
 import '@fortawesome/fontawesome-free/js/all'
 import "@fortawesome/fontawesome-free/css/all.min.css"
+
+var fr = document.querySelector('form');
+fr.addEventListener('submit', function (e) {
+    //fr.submit();
+    e.preventDefault();
+});
